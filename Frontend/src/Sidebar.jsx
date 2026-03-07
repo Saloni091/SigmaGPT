d4a68d855f9ecbaf7db04db7342ba7430548af89
Saloni-01 <Saloni198400@gmail.com>
2026-03-07 22:46:28 +0530

    connect Frontend with Backend

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -10,7 +10,7 @@ function Sidebar() {
 
  const getAllThreads = async () => {
         try {
-            const response = await fetch("http://sigmagpt-1-vocn.onrender.com/api/thread");
+            const response = await fetch("https://sigmagpt-1-vocn.onrender.com/api/thread");
             const res = await response.json();
             const filteredData = res.map(thread => ({threadId: thread.threadId, title: thread.title}));
             //console.log(filteredData);
