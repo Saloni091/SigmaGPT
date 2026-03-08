9aedb92c4c3c7e14b07dc24258e7ed68e5399169
Saloni-01 <Saloni198400@gmail.com>
2026-03-08 12:51:58 +0530

    fix port andcors for deployment

--- Backend/server2.js
+++ Backend/server2.js
@@ -1,16 +1,21 @@
-import express from "express";
-import"dotenv/config";
 import cors from "cors";
+import dns from "dns";
+import "dotenv/config";
+import express from "express";
 import mongoose from "mongoose";
 import chatRoutes from "./routes/chat.js";
-import dns from "dns";
 
 const app = express();
-const PORT = 8080;
+// const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 dns.setServers(["8.8.8.8"]); // Set Google's DNS server
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(express.json());
 
 app.use("/api", chatRoutes);
